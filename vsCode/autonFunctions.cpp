24810b7eaaf36350532139456f3c1008619676ef
Meredith Green <44354558+aerhoespace@users.noreply.github.com>
2019-01-31 12:22:26 -0700

    Add files via upload

--- vsCode/autonFunctions.cpp
+++ vsCode/autonFunctions.cpp
@@ -1,7 +1,12 @@
 /*---------------------------------------------------------------------------*/
+/*                                                                           */
 /*                           Autonomous Functions                            */
+/*                                                                           */
 /*---------------------------------------------------------------------------*/
 
+/*---------------------------------------------------------------------------*/
+/*                       Autonomous Drive Functions                          */
+/*---------------------------------------------------------------------------*/
 double sgn(double number){
     if (number > 0) number = 1;
     else number = -1;
@@ -34,10 +39,10 @@ void setRightBackPower (int power){
 }
 
 void setDrivePower (int left, int right){
-    setRightBackPower(right);
-    setRightFrontPower(right);
-    setLeftBackPower(left);
     setLeftFrontPower(left);
+    setLeftBackPower(left);
+    setRightFrontPower(right);
+    setRightBackPower(right);
 }
 
 void autoDrive(int distance, int power = 100){
@@ -66,4 +71,69 @@ void autoTurn(int distance, int power = 100){
         setDrivePower(power*direction, -power*direction);
     }
     setDrivePower(0,0);
+}
+
+/*---------------------------------------------------------------------------*/
+/*                       Autonomous Intake Functions                         */
+/*---------------------------------------------------------------------------*/
+void setIntakeLeftPower (int power){
+    if(power==0) intakeLeft.stop(vex::brakeType::coast);
+    else{
+        intakeLeft.spin(vex::directionType::fwd,power,vex::velocityUnits::pct);
+    }
+}
+void setIntakeRightPower (int power){
+    if(power==0) intakeRight.stop(vex::brakeType::coast);
+    else{
+        intakeRight.spin(vex::directionType::fwd,power,vex::velocityUnits::pct);
+    }
+}
+void setIntakePower (int left, int right){
+    setIntakeLeftPower(left);
+    setIntakeRightPower(right);
+}
+
+void autoIn(int rotations, int power = 100){
+    int direction = sgn(rotations);
+        
+    intakeLeft.resetRotation();
+    intakeRight.resetRotation();
+    
+    while(std::abs(intakeLeft.rotation(vex::rotationUnits::deg))<std::abs(rotations)){
+        setIntakePower(power*direction, power*direction);
+    }
+    setIntakePower(0,0);
+}
+
+/*---------------------------------------------------------------------------*/
+/*                      Autonomous Puncher Functions                         */
+/*---------------------------------------------------------------------------*/
+void setPuncherLeftPower (int power){
+    if(power==0) puncherLeft.stop(vex::brakeType::coast);
+    else{
+        puncherLeft.spin(vex::directionType::fwd,power,vex::velocityUnits::pct);
+    }
+}
+void setPuncherRightPower (int power){
+    if(power==0) puncherRight.stop(vex::brakeType::coast);
+    else{
+        puncherRight.spin(vex::directionType::fwd,power,vex::velocityUnits::pct);
+    }
+}
+
+void setPuncherPower (int left, int right){
+    setPuncherLeftPower(left);
+    setPuncherRightPower(right);
+}
+
+void autoShoot(int rotations, int power = 100){
+    int direction = sgn(rotations);
+        
+    puncherLeft.resetRotation();
+    puncherRight.resetRotation();
+    
+    while(std::abs(intakeLeft.rotation(vex::rotationUnits::deg))<std::abs(rotations)){
+        setPuncherPower(power*direction, power*direction);
+    }
+    setPuncherPower(0,0);
 }
