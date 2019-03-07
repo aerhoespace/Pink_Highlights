d16c8d77a2fe584a5efe8059039c609c297386a9
Acetousk <acetousk@users.noreply.github.com>
2019-03-07 09:01:14 -0700

    Update opcontrol.cpp

--- PROS/src/opcontrol.cpp
+++ PROS/src/opcontrol.cpp
@@ -6,16 +6,7 @@
 #define MOTOR_PORT 16 //intake
 
 void opcontrol() {
-  //setup controller
-  pros::Controller master (E_CONTROLLER_MASTER);
-  
-  //setup motors
-  pros::Motor leftFront (11); //all of these take additional conditions check: https://pros.cs.purdue.edu/v5/tutorials/topical/motors.html
-  pros::Motor leftBack (12);
-  pros::Motor rightFront (13);
-  pros::Motor rightBack (14);
-  pros::Motor puncher (15);
-  pros::Motor intake (16);
+
   
   
   while (true) {
