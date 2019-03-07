8358a569fb6db5b15c465e31d851a542e8f498ad
Acetousk <acetousk@users.noreply.github.com>
2019-03-07 12:06:43 -0700

    Update initialize.cpp

--- PROS/src/initialize.cpp
+++ PROS/src/initialize.cpp
@@ -13,15 +13,11 @@ void initialize() {
   pros::Controller master (E_CONTROLLER_MASTER);
   
   //setup motors
-  pros::Motor leftFront  (11, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES); //all of these take additional conditions check: https://pros.cs.purdue.edu/v5/tutorials/topical/motors.html
+  pros::Motor leftFront  (11, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES); //for conditions check: https://pros.cs.purdue.edu/v5/tutorials/topical/motors.html
   pros::Motor leftBack   (12, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);  //E_MOTOR_GEARSET_36 = red(100RPM),	E_MOTOR_GEARSET_18 == green(200 RPM), E_MOTOR_GEARSET_06 = blue(600 RPM) for more see: https://pros.cs.purdue.edu/v5/api/c/motors.html#motor-gearset-e-t 	 
   pros::Motor rightFront (13, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
   pros::Motor rightBack  (14, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
   pros::Motor puncher    (15, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
   pros::Motor intake     (16, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
   
-  //setup motors
-  pros::Motor left (11, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);//11th port 
-
-
 }
