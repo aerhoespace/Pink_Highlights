e3aadd2af59f71bf13e0aab9c0125f74a97719b6
Acetousk <acetousk@users.noreply.github.com>
2019-03-07 12:26:33 -0700

    Update main.h

--- PROS/include/main.h
+++ PROS/include/main.h
@@ -2,9 +2,25 @@
 
 // Motivational quote
 void littleCeasars(){
-  pros::lcd::set_text(1, "lEtS gEt tHiS bReAd");
+  //will display text on controller converted from VCS folder
+  pros::lcd::set_text(1, "lEtS gEt tHiS bReAd");  //for documentation check: https://pros.cs.purdue.edu/v5/api/cpp/llemu.html or https://pros.cs.purdue.edu/v5/api/cpp/misc.html#set-text
+                              
 }
 
 void opDrive(){
-  //
+  //simple tank drive code for opControl
+  leftFront.move(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y)); //for documentation check: https://pros.cs.purdue.edu/v5/api/cpp/motors.html#move-velocity or https://pros.cs.purdue.edu/v5/tutorials/topical/controller.html?highlight=controller%20analog
+  leftBack.move(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y));  //for docs check: https://pros.cs.purdue.edu/v5/tutorials/topical/motors.html
+  rightFront.move(master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y));
+  rightBack.move(master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y));
 }  
+
+void opIntake(){
+  if(master.get_digital(E_CONTROLLER_DIGITAL_R2)){       //for docs check: https://pros.cs.purdue.edu/v5/api/c/misc.html#controller-id-e-t
+    intake.move(100);
+  }else if(master.get_digital(E_CONTROLLER_DIGITAL_L1)){
+    intake.move(-100);
+  }else{
+    intake.move(0);
+  }    
+}
